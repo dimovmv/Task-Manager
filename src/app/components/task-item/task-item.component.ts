c0b824b2a0cf4d14c49eae8d061438737ab4949b
dimovmv <dimidimov.mv@gmail.com>
2022-12-11 20:25:55 +0000

    initial commit

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css'],
+})
+export class TaskItemComponent {
+  @Input() task: Task | undefined;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+  faTimes = faTimes;
+
+  // ngOnInit(): void {}
+
+  onDelete(task: Task | undefined) {
+    this.onDeleteTask.emit(task);
+    console.log(task);
+  }
+
+  onToggle(task: Task | undefined) {
+    this.onToggleReminder.emit(task);
+  }
+}
